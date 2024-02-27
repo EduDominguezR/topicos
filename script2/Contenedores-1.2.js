c75dbb19325cf08102f8ba55f27ad694ad259145
EduDominguezR <158128274+EduDominguezR@users.noreply.github.com>
2024-02-26 16:07:52 -0800

    Add files via upload

---
+++ script2/Contenedores-1.2.js
@@ -0,0 +1,26 @@
+const colores = ["red", "purple", "cyan", "orange", "green", "yellow", "blue", "pink",
+                 "gray", "brown", "black", "white", "maroon", "fuchsia", "lime", "turquoise",
+                 "violet", "salmon", "beige", "gold", "red", "#168056", "#1DC281","#A47EA0","#B1B73B"];
+
+
+const usados = [];
+const body =document.querySelector('body')
+
+for (let i = 0; i < 10; i++) {
+  const div = document.createElement("div");
+  div.style.height = "100px";
+
+  let color;
+  do {
+    color = colores[Math.floor(Math.random() * colores.length)];
+  } while (usados.includes(color)); 
+  usados.push(color); 
+
+  div.style.backgroundColor = color;
+  div.textContent = i + 1;
+
+  body.appendChild(div);
+}
+
+
+  
