bb0c46e2152c94f0702b576252f98b7f3648edc4
EduDominguezR <158128274+EduDominguezR@users.noreply.github.com>
2024-02-26 16:10:28 -0800

    Add files via upload

---
+++ script3/parrafos-inteligente.js
@@ -0,0 +1,14 @@
+const caracteres = 'Zapatos de Gucci, pantalón Versace, chamarra de piel,Perfume de Dolce Gabbana y mañana traigo mucha lanacdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@#$%^&*()_-+=[]{}|;:,.<>?';
+
+for (let i = 0; i < 5; i++) {
+  const tamañoMinimo = 50;
+  const tamañoMax = 100;
+  const tamaño = Math.floor(Math.random() * (tamañoMax - tamañoMinimo + 1)) + tamañoMinimo;
+  let texto = '';
+  for (let e = 0; e < tamaño; e++) {
+    texto += caracteres[Math.floor(Math.random() * caracteres.length)];
+  }
+  console.log(`Párrafo ${i + 1}: ${texto}`);
+  console.log(`tamaño: ${texto.length} caracteres`);
+  console.log();
+}
