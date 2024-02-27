4de1acb2557c0d0e97ac574029dd7498b3ad6777
EduardoDominguezReyes <158128274+EduDominguezR@users.noreply.github.com>
2024-02-26 17:42:21 -0800

    Update contenedores.js

--- script1/contenedores.js
+++ script1/contenedores.js
@@ -7,7 +7,7 @@ parrafo.textContent='soy un parrafo'
 parrafo.style.fontSize='30px'
 body.appendChild(div)
 div.appendChild(parrafo)*/
-const colores = ["red", "purple", "cian", "orange", "green","pink","#C23A1D","#1DC281","gold"]
+const colores = ["red", "purple", "cian", "orange", "green","pink","#C23A1D","#1DC281","gold","white"]
 
 const body =document.querySelector('body')
 for (let i = 0; i < 10; i++) {
