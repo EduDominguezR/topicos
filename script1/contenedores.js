7519b4b4f9a6f9a4477ea1e88eacc615533f7855
EduDominguezR <158128274+EduDominguezR@users.noreply.github.com>
2024-02-26 16:06:34 -0800

    Add files via upload

---
+++ script1/contenedores.js
@@ -0,0 +1,35 @@
+/*const body =document.querySelector('body')
+const div =document.createElement('div')
+div.style.height='500px'
+div.style.backgroundColor=colores[Math.floor(Math.random() * colores.length)];
+const parrafo=document.createElement('p')
+parrafo.textContent='soy un parrafo'
+parrafo.style.fontSize='30px'
+body.appendChild(div)
+div.appendChild(parrafo)*/
+const colores = ["red", "purple", "cian", "orange", "green","pink","#C23A1D","#1DC281","gold"]; 
+
+const body =document.querySelector('body')
+for (let i = 0; i < 10; i++) {
+  const div = document.createElement("div");
+
+  div.style.height = "100px";
+  div.style.backgroundColor = colores[Math.floor(Math.random() * colores.length)];
+
+  div.textContent = i + 1;
+
+  
+body.appendChild(div);
+}
+
+
+
+
+
+    /*parrafo.style.textAlign='center'
+    parrafo.style.fontSize='20px'
+    parrafo.style.fontWeight='bold'
+    div.appendChild(parrafo)
+    body.appendChild(div)*/
+
+
