aa3d97be4b267a13ee75f7ddee1064a3c077d954
EduardoDominguezReyes <158128274+EduDominguezR@users.noreply.github.com>
2024-02-26 17:37:32 -0800

    Update Contenedores-1.2.js

--- script2/Contenedores-1.2.js
+++ script2/Contenedores-1.2.js
@@ -1,25 +1,25 @@
 const colores = ["red", "purple", "cyan", "orange", "green", "yellow", "blue", "pink",
                  "gray", "brown", "black", "white", "maroon", "fuchsia", "lime", "turquoise",
-                 "violet", "salmon", "beige", "gold", "red", "#168056", "#1DC281","#A47EA0","#B1B73B"];
+                 "violet", "salmon", "beige", "gold", "red", "#168056", "#1DC281","#A47EA0","#B1B73B"]
 
 
-const usados = [];
+const usados = []
 const body =document.querySelector('body')
 
 for (let i = 0; i < 10; i++) {
-  const div = document.createElement("div");
-  div.style.height = "100px";
+  const div = document.createElement("div")
+  div.style.height = "100px"
 
-  let color;
+  let color
   do {
-    color = colores[Math.floor(Math.random() * colores.length)];
-  } while (usados.includes(color)); 
-  usados.push(color); 
+    color = colores[Math.floor(Math.random() * colores.length)]
+  } while (usados.includes(color))
+  usados.push(color)
 
-  div.style.backgroundColor = color;
-  div.textContent = i + 1;
+  div.style.backgroundColor = color
+  div.textContent = i + 1
 
-  body.appendChild(div);
+  body.appendChild(div)
 }
 
 
